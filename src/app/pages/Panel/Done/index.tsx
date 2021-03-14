1ecc78a35d7acf16317ad700aae3943f28468b2f
Daniel Jorge <djpgonzaga@gmail.com>
2021-03-14 02:04:59 -0300

    feat: tdd for done taska, undone and delete actions

--- src/app/pages/Panel/Done/index.tsx
+++ src/app/pages/Panel/Done/index.tsx
@@ -1,7 +1,47 @@
-const Done = () => (
-  <div>
-    Done
-  </div>
-);
+import DeleteIcon from '../../../common/components/DeleteIcon';
+import DoneIcon from '../../../common/components/DoneIcon';
+import { useDoneTask } from '../../../common/context/TaskContext';
+
+const Done = () => {
+  const [done, dispatch] = useDoneTask();
+
+  function handleUndoneTask(id: string) {
+    dispatch({ type: 'undone', id });
+  }
+
+  function handleDeleteTask(id: string) {
+    dispatch({ type: 'delete', id });
+  }
+
+  return (
+    <div>
+      <div>
+        <ul aria-label="done tasks">
+          {done.map((task) => (
+            <li key={task.id}>
+              <p>
+                {task.description}
+              </p>
+              <button
+                type="button"
+                aria-label={`undone task ${task.description}`}
+                onClick={() => handleUndoneTask(task.id)}
+              >
+                <DoneIcon />
+              </button>
+              <button
+                type="button"
+                aria-label={`delete task ${task.description}`}
+                onClick={() => handleDeleteTask(task.id)}
+              >
+                <DeleteIcon />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Done;
--- src/app/pages/Panel/index.integration.test.tsx
+++ src/app/pages/Panel/index.integration.test.tsx
@@ -58,3 +58,35 @@ test('delete a todo tasks', async () => {
   expect(within(done).queryByText(/clean the house/i)).not.toBeInTheDocument();
 });
 
+test('undone a finish task', async () => {
+  renderWithProvider(<App />);
+
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'make dinner');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  const todo = screen.getByRole('list', { name: /todo tasks/i });
+  const done = screen.getByRole('list', { name: /done tasks/i });
+
+  userEvent.click(screen.getByRole('button', { name: /complete task make dinner/i }));
+  userEvent.click(screen.getByRole('button', { name: /undone task make dinner/i }));
+
+  expect(within(todo).getByText(/make dinner/i)).toBeInTheDocument();
+  expect(within(done).queryByText(/make dinner/i)).not.toBeInTheDocument();
+});
+
+test('delete a finish tasks', async () => {
+  renderWithProvider(<App />);
+
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'clean the house');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  userEvent.click(screen.getByRole('button', { name: /complete task clean the house/i }));
+
+  const todo = screen.getByRole('list', { name: /todo tasks/i });
+  const done = screen.getByRole('list', { name: /done tasks/i });
+
+  userEvent.click(screen.getByRole('button', { name: /delete task clean the house/i }));
+
+  expect(within(todo).queryByText(/clean the house/i)).not.toBeInTheDocument();
+  expect(within(done).queryByText(/clean the house/i)).not.toBeInTheDocument();
+});
