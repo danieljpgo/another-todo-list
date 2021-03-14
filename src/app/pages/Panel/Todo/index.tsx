d48a483e03780e9a3573687b9b0d836caa923081
Daniel Jorge <djpgonzaga@gmail.com>
2021-03-14 02:04:12 -0300

    feat: tdd for todo task, add, finish and delete actions

--- src/app/pages/Panel/Todo/index.tsx
+++ src/app/pages/Panel/Todo/index.tsx
@@ -1,7 +1,69 @@
-const Todo = () => (
-  <div>
-    Todo
-  </div>
-);
+import * as React from 'react';
+import DeleteIcon from '../../../common/components/DeleteIcon';
+import DoneIcon from '../../../common/components/DoneIcon';
+import { useTodoTask } from '../../../common/context/TaskContext';
+
+const Todo = () => {
+  const [input, setInput] = React.useState('');
+  const [todo, dispatch] = useTodoTask();
+
+  function handleDoneTask(id: string) {
+    dispatch({ type: 'done', id });
+  }
+
+  function handleDeleteTask(id: string) {
+    dispatch({ type: 'delete', id });
+  }
+
+  function handleSubmit(e:React.FormEvent<HTMLFormElement>, description: string) {
+    e.preventDefault();
+    dispatch({ type: 'add', description });
+    setInput('');
+  }
+
+  return (
+    <div>
+      <ul aria-label="todo tasks">
+        {todo.map((task) => (
+          <li key={task.id}>
+            <p>{task.description}</p>
+            <button
+              type="button"
+              title="complete task"
+              aria-label={`complete task ${task.description}`}
+              className="w-2"
+              onClick={() => handleDoneTask(task.id)}
+            >
+              <DoneIcon />
+            </button>
+            <button
+              type="button"
+              title="delete task"
+              aria-label={`delete task ${task.description}`}
+              className="w-2"
+              onClick={() => handleDeleteTask(task.id)}
+            >
+              <DeleteIcon />
+            </button>
+          </li>
+        ))}
+      </ul>
+      {!todo.length && <p>No task todo</p>}
+      <div>
+        <form onSubmit={(e) => handleSubmit(e, input)}>
+          <label htmlFor="new_task">a</label>
+          <input
+            id="new_task"
+            name="new_task"
+            type="text"
+            value={input}
+            placeholder="new task"
+            onChange={(e) => setInput(e.currentTarget.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Todo;
---
+++ src/app/pages/Panel/index.integration.test.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { TaskProvider } from '../../common/context/TaskContext';
+import App from '../..';
+
+function renderWithProvider(children: React.ReactNode) {
+  render(<TaskProvider>{children}</TaskProvider>);
+}
+
+test('add new task', () => {
+  renderWithProvider(<App />);
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'make dinner');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  const todo = screen.getByRole('list', { name: /todo task/i });
+
+  expect(within(todo).getByText(/make dinner/i)).toBeInTheDocument();
+});
+
+test('finish a new task', async () => {
+  renderWithProvider(<App />);
+
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'make dinner');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'clean the house');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  userEvent.click(screen.getByRole('button', { name: /complete task make dinner/i }));
+
+  const todo = screen.getByRole('list', { name: /todo tasks/i });
+  const done = screen.getByRole('list', { name: /done tasks/i });
+
+  expect(within(todo).queryByText(/make dinner/i)).not.toBeInTheDocument();
+  expect(within(todo).getByText(/clean the house/i)).toBeInTheDocument();
+
+  expect(within(done).getByText(/make dinner/i)).toBeInTheDocument();
+});
+
+test('delete a todo tasks', async () => {
+  renderWithProvider(<App />);
+
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'make dinner');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  userEvent.type(screen.getByPlaceholderText(/new task/i), 'clean the house');
+  userEvent.type(screen.getByPlaceholderText(/new task/i), '{enter}');
+
+  userEvent.click(screen.getByRole('button', { name: /delete task clean the house/i }));
+
+  const todo = screen.getByRole('list', { name: /todo tasks/i });
+  const done = screen.getByRole('list', { name: /done tasks/i });
+
+  expect(within(todo).getByText(/make dinner/i)).toBeInTheDocument();
+  expect(within(todo).queryByText(/clean the house/i)).not.toBeInTheDocument();
+
+  expect(within(done).queryByText(/clean the house/i)).not.toBeInTheDocument();
+});
+
