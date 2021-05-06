6418538e04515b298e618ff073afca764ccce616
Daniel Jorge <djpgonzaga@gmail.com>
2021-05-06 01:37:43 -0300

    feat: gradient background

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { TaskProvider } from './common/context/TaskContext';
 import Panel from './pages/Panel';
 
 const App = () => (
-  <main className="grid h-screen place-items-center">
+  <main className="grid h-screen place-items-center bg-gradient-to-tr from-gray-200 via-gray-200 to-blue-200">
     <TaskProvider>
       <Panel />
     </TaskProvider>
